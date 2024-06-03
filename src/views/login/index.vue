9bdbe24a0129138b7500dab4692ad7d78261560a
wen shiqiang <53844499+wen-shiqiang@users.noreply.github.com>
2024-06-03 15:19:53 +0800

    删除无用代码

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -6,8 +6,8 @@ import { loginRules } from "./utils/rule";
 import { useNav } from "@/layout/hooks/useNav";
 import type { FormInstance } from "element-plus";
 import { useLayout } from "@/layout/hooks/useLayout";
-import { useUserStoreHook } from "@/store/modules/user";
-import { initRouter, getTopMenu } from "@/router/utils";
+// import { useUserStoreHook } from "@/store/modules/user";
+// import { initRouter, getTopMenu } from "@/router/utils";
 import { bg, avatar, illustration } from "./utils/static";
 import { useRenderIcon } from "@/components/ReIcon/src/hooks";
 import { ref, reactive, toRaw, onMounted, onBeforeUnmount } from "vue";
@@ -17,7 +17,9 @@ import dayIcon from "@/assets/svg/day.svg?component";
 import darkIcon from "@/assets/svg/dark.svg?component";
 import Lock from "@iconify-icons/ri/lock-fill";
 import User from "@iconify-icons/ri/user-3-fill";
-
+import { setToken } from "@/utils/auth";
+import { addPathMatch } from "@/router/utils";
+import { usePermissionStoreHook } from "@/store/modules/permission";
 defineOptions({
   name: "Login"
 });
@@ -40,24 +42,37 @@ const ruleForm = reactive({
 const onLogin = async (formEl: FormInstance | undefined) => {
   if (!formEl) return;
   await formEl.validate((valid, fields) => {
+    loading.value = true;
     if (valid) {
-      loading.value = true;
-      useUserStoreHook()
-        .loginByUsername({ username: ruleForm.username, password: "admin123" })
-        .then(res => {
-          if (res.success) {
-            // 获取后端路由
-            return initRouter().then(() => {
-              router.push(getTopMenu(true).path).then(() => {
-                message("登录成功", { type: "success" });
-              });
-            });
-          } else {
-            message("登录失败", { type: "error" });
-          }
-        })
-        .finally(() => (loading.value = false));
+      setToken({
+        username: "admin",
+        roles: ["admin"],
+        accessToken: "eyJhbGciOiJIUzUxMiJ9.admin"
+      } as any);
+      // 全部采取静态路由模式
+      usePermissionStoreHook().handleWholeMenus([]);
+      addPathMatch();
+      router.push("/");
+      message("登录成功", { type: "success" });
     }
+    // if (valid) {
+    // loading.value = true;
+    // useUserStoreHook()
+    //   .loginByUsername({ username: ruleForm.username, password: "admin123" })
+    //   .then(res => {
+    //     if (res.success) {
+    //       // 获取后端路由
+    //       return initRouter().then(() => {
+    //         router.push(getTopMenu(true).path).then(() => {
+    //           message("登录成功", { type: "success" });
+    //         });
+    //       });
+    //     } else {
+    //       message("登录失败", { type: "error" });
+    //     }
+    //   })
+    //   .finally(() => (loading.value = false));
+    // }
   });
 };
 
